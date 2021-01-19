954e2c717e8ff0e25b5f365c96d52a1e7e435dcc
koknaev <89024464050@yandex.ru>
2021-01-19 23:31:23 +0500

    1

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,34 @@
 package com.udacity.asteroidradar.main
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.asteroidradar.repository.AsteroidsRepository
+import java.lang.IllegalArgumentException
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database = getDatabase(application)
+
+    private fun getDatabase(application: Application): Any {
+        return 0
+    }
+
+    private val asteroidsRepository = AsteroidsRepository(database)
+
+    val asteroids = asteroidsRepository.asteroids
+
+
+    class Factory(private val app:Application): ViewModelProvider.Factory{
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return MainViewModel(app) as T
+            }
+            throw IllegalArgumentException("unable to construct MainViewModel")
+        }
+    }
 }
+
+

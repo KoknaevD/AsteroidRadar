6200fef4ed8e958ef67bd600a3c04c72b1a274ea
koknaev <89024464050@yandex.ru>
2021-01-25 20:42:36 +0500

    4

--- app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
@@ -55,3 +55,18 @@ private fun getNextSevenDaysFormattedDates(): ArrayList<String> {
 
     return formattedDateList
 }
+
+fun getCurrentDay(): String {
+    val currentTime = Calendar.getInstance().time
+    val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+
+    return dateFormat.format(currentTime)
+}
+ fun getNextWeekDay(): String {
+    val calendar = Calendar.getInstance()
+    calendar.add(Calendar.DAY_OF_YEAR, 7)
+    val currentTime = calendar.time
+    val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+
+    return dateFormat.format(currentTime)
+}
